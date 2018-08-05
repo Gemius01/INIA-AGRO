4037dcb7863cc0cab4b4006a31e397101fa46d4c
ivar29 <ivar_zavala29@hotmail.com>
2018-08-04 21:57:11 -0300

    limpieza

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,21 +20,13 @@ use App\Http\Requests\UserEditByUserRequest;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
 
-     */
     public function setEnvironmentValue($envKey, $envValue)
     {
         $envFile = app()->environmentFilePath();
         $str = file_get_contents($envFile);
-
         $oldValue = strtok($str, "{$envKey}=");
-
         $str = str_replace("{$envKey}={$oldValue}", "{$envKey}={$envValue}\n", $str);
-
         $fp = fopen($envFile, 'w');
         fwrite($fp, $str);
         fclose($fp);
@@ -42,42 +34,21 @@ class UserController extends Controller
 
     public function index()
     {
-        //$users = User::paginate();
-        //$users = User::with('region_id')->paginate(20);
-
-
         $users = User::where('id', '<>', 1)->with('secciones')->get();
-
-        //dd($users);
-        //$regions = Region::get();
-        //return $users;
         return view('users.index', compact('users'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         $roles = Role::get();
         $seccions = Seccion::get();
-        //$regions = Region::pluck('name', 'id');
         $regions = Region::get();
         $macrozonas = Macrozona::with('rubro')->get();
-        //dd($macrozonas);
         return view('users.create', compact([
             'regions', 'roles', 'seccions', 'macrozonas',
         ]));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(UserStoreRequest $request)
     {
 
@@ -100,31 +71,22 @@ class UserController extends Controller
             'password'  => Hash::make($request['password']),
             'cargo'     => $request['cargo'],
         ]);
-
-        //\Mail::to('pablo.gemio01@gmail.com')->send(new CreateUser);
         //Sincroniza las tablas intermedias de macrozona_user
         $user->regiones()->sync($request->get('regions'));
-
         //Sincroniza las tablas intermedias de macrozona_user
         $user->macrozonas()->sync($request->get('macrozonas'));
-
         //Sincroniza las tablas intermedias de role_user
         $user->roles()->sync($request->get('roles'));
-
         //Sincroniza las tablas intermedias de seccion_user
         $user->secciones()->sync($request->get('secciones'));
-
         $secciones = $request->get('secciones');
-
         if($secciones != null)
         {
-
             foreach($secciones as $seccion)
                 {
                     $permission = Permission::where('slug', 'seccion-'.$seccion)->first();
                     $user->permissions()->attach($permission);
                 }
-
         }
 
         return redirect()->route('users.index', $user->id)
@@ -137,46 +99,29 @@ class UserController extends Controller
             'cri'       => $request['cri'],
             'password'  => Hash::make($request['password']),
             'cargo'     => $request['cargo'],
-
-
         ]);
-
-        //\Mail::to('pablo.gemio01@gmail.com')->send(new CreateUser);
         //Sincroniza las tablas intermedias de macrozona_user
         $user->regiones()->sync($request->get('regions'));
-
         //Sincroniza las tablas intermedias de macrozona_user
         $user->macrozonas()->sync($request->get('macrozonas'));
-
         //Sincroniza las tablas intermedias de role_user
         $user->roles()->sync($request->get('roles'));
-
         //Sincroniza las tablas intermedias de seccion_user
         $user->secciones()->sync($request->get('secciones'));
-
         $secciones = $request->get('secciones');
-
         if($secciones != null)
         {
-
             foreach($secciones as $seccion)
                 {
                     $permission = Permission::where('slug', 'seccion-'.$seccion)->first();
                     $user->permissions()->attach($permission);
                 }
-
         }
 
         return redirect()->route('users.index', $user->id)
             ->with('info', 'Usuario registrado con exito');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\User  $User
-     * @return \Illuminate\Http\Response
-     */
     public function show($idUser)
     {
         $user = User::find(decrypt($idUser));
@@ -188,53 +133,22 @@ class UserController extends Controller
         return view('users.show', compact('user', 'roles', 'seccions', 'regiones', 'macrozonas'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\User  $User
-     * @return \Illuminate\Http\Response
-     */
     public function edit(User $user)
     {
-        //$user = User::find($user->id)->secciones;
         $roles = Role::get();
         $regiones = Region::get();
-        //$seccions = Seccion::get();
-
-        //$checkeds = User::find($user->id)->secciones;
-        //return $checkeds;
         return view('users.edit', compact(['user', 'roles', 'regiones', ]));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\User  $User
-     * @return \Illuminate\Http\Response
-     */
     public function update(UserUpdateRequest $request, User $user)
     {
-
         $user->roles()->sync($request->get('roles'));
-
-
-        //$user->secciones()->sync($request->get('secciones'));
-
         $user->regiones()->sync($request->get('regiones'));
-
         $user->update($request->all());
-        //$user->roles()->sync($request->get('seccions'));
         return redirect()->route('users.edit', $user->id)
             ->with('info', 'Usuario actualizado con exito');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\User  $User
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(User $user)
     {
         $user->delete();
@@ -242,20 +156,19 @@ class UserController extends Controller
     }
     public function vistaMacrozonas(User $user)
     {
-
         $macrozonasUser = $user->macrozonas;
         if($user->regiones()->exists())
         {
             if($user->regiones()->first()->id != 1)
             {
                 $ids = [];
-                    foreach ($user->regiones as $region) 
+                    foreach ($user->regiones as $region)
                     {
                         $ids[] = $region['id'];
                     }
                 $macrozonas = Macrozona::whereIn('region_id', $ids)->orderBy('region_id', 'asc')->get();
             }else{
-              
+
               $macrozonas = Macrozona::orderBy('region_id', 'asc')->get();
             }
         }else{
@@ -263,18 +176,15 @@ class UserController extends Controller
          $macrozonas = array();
         }
 
-        //dd($macrozonas);
         return view('users.macrozonas', compact(['user', 'macrozonasUser', 'macrozonas', ]));
     }
 
     public function agregarMacrozonas(Request $request, User $user)
     {
-        //return view('users.macrozonas', compact(['user',]));
         $user->macrozonas()->sync($request->get('macrozonas'));
         $permission = array();
         $macrozonas = $request->get('macrozonas');
         $secciones = $user->secciones()->pluck('seccion_id');
-        //dd($secciones);
         if($secciones != null)
         {
             foreach($secciones as $seccion)
@@ -298,7 +208,6 @@ class UserController extends Controller
 
     public function vistaSecciones(User $user)
     {
-
         $secciones = Seccion::get();
         $seccionesUser = $user->secciones;
         return view('users.secciones', compact(['user', 'seccionesUser', 'secciones', ]));
@@ -306,11 +215,8 @@ class UserController extends Controller
 
     public function agregarSecciones(Request $request, User $user)
     {
-
         $user->secciones()->sync($request->get('secciones'));
-
         $secciones = $request->get('secciones');
-
         $permission = array();
         $macrozonas = $user->macrozonas()->pluck('macrozona_id');
         if($macrozonas != null)
@@ -343,7 +249,6 @@ class UserController extends Controller
     {
         $user->password = Hash::make($request['password']);
         $user->save();
-
         return redirect()->route('users.index', $user->id)
             ->with('info', 'Se ha actualizado el usuario '.$user->name);
     }
