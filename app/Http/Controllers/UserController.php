1bfa6c819ad40bdfce388528c28baf20966b7d93
Gemius01 <pablo.gemio@inacapmail.cl>
2018-08-12 19:45:47 -0300

    quitar msj al no enviar el correo

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -137,7 +137,7 @@ class UserController extends Controller
         }
 
         return redirect()->route('users.index', $user->id)
-            ->with('info', 'Se ha creado el Usuario pero no se logró enviar el correo '.$e->getMessage());
+            ->with('info', 'Se ha creado el Usuario pero no se logró enviar el correo ');
         }
 
 
