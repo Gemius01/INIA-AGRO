31d197a1ff8f3e77eb8d94c85f6dd1a677ba1c03
Gemius01 <pablo.gemio@inacapmail.cl>
2018-12-06 16:42:07 -0300

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,21 +11,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(PermissionsTableSeeder::class);
-        $this->call(RolesSeeder::class);
-        $this->call(RootUserSeeder::class);
+        //$this->call(PermissionsTableSeeder::class);
+        //$this->call(RolesSeeder::class);
+        //$this->call(RootUserSeeder::class);
         //$this->call(UsersTableSeeder::class);
-        $this->call(CountriesTableSeeder::class);
-        $this->call(RegionsTableSeeder::class);
-        $this->call(SeccionsTableSeeder::class);
-        $this->call(RubrosTableSeeder::class);
-        $this->call(MacrozonasTableSeeder::class);
-        $this->call(ColaboradorPermissionsSeeder::class);
-        $this->call(EditorPermissionsSeeder::class);
-        $this->call(EncargadoPermissionsSeeder::class);
-        $this->call(EleccionTableSeeder::class);
-        $this->call(MesTableSeeder::class);
-        $this->call(ContadorVisitasSeeder::class);
+        //$this->call(CountriesTableSeeder::class);
+        //$this->call(RegionsTableSeeder::class);
+        //$this->call(SeccionsTableSeeder::class);
+        // $this->call(RubrosTableSeeder::class);
+        // $this->call(MacrozonasTableSeeder::class);
+        // $this->call(ColaboradorPermissionsSeeder::class);
+        // $this->call(EditorPermissionsSeeder::class);
+        // $this->call(EncargadoPermissionsSeeder::class);
+        // $this->call(EleccionTableSeeder::class);
+        // $this->call(MesTableSeeder::class);
+        // $this->call(ContadorVisitasSeeder::class);
         $this->call(ComunaTableSeeder::class);
     }
 }
